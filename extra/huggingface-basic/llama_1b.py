9a72a8d86603ca468107233fecf59cdd57e51092
maslovm <mmas6150@yandex.ru>
2025-10-31 10:17:19 +0000

    docs: update usage example

--- extra/huggingface-basic/llama_1b.py
+++ extra/huggingface-basic/llama_1b.py
@@ -5,16 +5,29 @@ from tinyquant.utils import quantize_matching_linear_layers
 
 
 def main() -> None:
+    model_id = "unsloth/Llama-3.2-1B"
+    model_dtype = torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float16
+
     model = transformers.AutoModelForCausalLM.from_pretrained(
-        "unsloth/Llama-3.2-1B",
-        torch_dtype=torch.bfloat16,
+        model_id,
+        device_map="auto",
+        dtype=model_dtype,
         low_cpu_mem_usage=True,
-        device_map="cuda",
+        attn_implementation="eager"
     )
-    tokenizer = transformers.AutoTokenizer.from_pretrained("unsloth/Llama-3.2-1B")
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_id)
+    device = model.get_input_embeddings().weight.device
+
+    # One-line quantization
     quantize_matching_linear_layers(model, "nf4", "model.layers.*.self_attn.q_proj")
+
+    prompt = "Quantization for neural networks helps with "
+    inputs = tokenizer(prompt, return_tensors="pt")["input_ids"].to(device)
+    
     output = model.generate(
-        tokenizer("", return_tensors="pt")["input_ids"].cuda(), max_new_tokens=1000
+        inputs, 
+        do_sample=True,
+        max_new_tokens=100
     )
     print(tokenizer.decode(output[0]))
 
