72a81ee04d1bb6c5d505f8c7fe0bc48a63d2cb2f
maslovm <mmas6150@yandex.ru>
2025-11-13 14:33:16 +0000

    add hqq

--- tinyquant/integrations/hqq.py
+++ tinyquant/integrations/hqq.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict, Any
+from typing import Any, Optional
 
 import torch
 import torch.nn as nn
@@ -25,51 +25,61 @@ class HQQQuantizer(DataFreeQuantizer):
         **extra_cfg: Any,
     ) -> "QuantizedLinear":
         try:
-            from hqq.core.quantize import HQQLinear, BaseQuantizeConfig
+            from hqq.core.quantize import BaseQuantizeConfig, HQQLinear
         except Exception as e:
             raise ImportError(
                 "HQQ is not installed. Please `pip install hqq>=0.2.1`."
             ) from e
-
+        # TODO: tests
         out_features, in_features = weight.shape
-        device = device or weight.device
-        compute_dtype = compute_dtype or weight.dtype
 
-        base = nn.Linear(in_features, out_features, bias=bias is not None, device=device, dtype=weight.dtype)
+        if device is None:
+            device = weight.device
+
+        if compute_dtype is None:
+            compute_dtype = weight.dtype
+
+        linear_base = nn.Linear(
+            in_features,
+            out_features,
+            bias=bias is not None,
+            device=device,
+            dtype=weight.dtype,
+        )
         with torch.no_grad():
-            base.weight.copy_(weight)
+            linear_base.weight.copy_(weight)
             if bias is not None:
-                base.bias.copy_(bias)
+                linear_base.bias.copy_(bias)
 
-        quant_config = BaseQuantizeConfig(nbits=nbits, group_size=group_size, **extra_cfg)
+        quant_config = BaseQuantizeConfig(
+            nbits=nbits, group_size=group_size, **extra_cfg
+        )
 
         hqq_layer = HQQLinear(
-            linear_layer=base,
+            linear_layer=linear_base,
             quant_config=quant_config,
             compute_dtype=compute_dtype,
             device=device,
             del_orig=True,
         )
 
-        state = hqq_layer.state_dict()
-        pd = nn.ParameterDict()
-        for k, v in state.items():
-            pd[k] = nn.Parameter(v, requires_grad=False)
+        state_dict = hqq_layer.state_dict()
+        state_dict_as_parameter_dict = nn.ParameterDict()
+        for k, v in state_dict.items():
+            state_dict_as_parameter_dict[k] = nn.Parameter(v, requires_grad=False)
 
-        qlin = QuantizedLinear.from_weights(
-            weights_dict=pd,
+        return QuantizedLinear.from_weights(
+            weights_dict=state_dict_as_parameter_dict,
             bias=None,
             quantization_method=HQQQuantizer.name(),
             in_features=in_features,
             out_features=out_features,
             meta={
-                    'config': quant_config,
-                    'device': str(device),
-                    'compute_dtype': str(compute_dtype).replace("torch.", ""),
-                },
+                "config": quant_config,
+                "device": str(device),
+                "compute_dtype": str(compute_dtype).replace("torch.", ""),
+            },
         )
-
-        return qlin
 
     @staticmethod
     def forward(linear: "QuantizedLinear", input_: torch.Tensor) -> torch.Tensor:
@@ -79,11 +89,13 @@ class HQQQuantizer(DataFreeQuantizer):
             raise ImportError(
                 "HQQ is not installed. Please `pip install hqq>=0.2.1`."
             ) from e
-        hqq_state = {k: v.data for k, v in linear.weights_dict.items() if k != 'meta'}
-        
+
+        hqq_state = {k: v.data for k, v in linear.weights_dict.items() if k != "meta"}
+
         hqq_cfg = linear.meta.get("meta", None)
         hqq_device = linear.meta.get("device", None)
         hqq_compute_dtype = linear.meta.get("compute_dtype", None)
+
         hqq_layer = HQQLinear(
             linear_layer=None,
             quant_config=hqq_cfg,
@@ -92,5 +104,6 @@ class HQQQuantizer(DataFreeQuantizer):
             del_orig=True,
             initialize=False,
         )
+
         hqq_layer.load_state_dict(hqq_state)
         return hqq_layer(input_)
